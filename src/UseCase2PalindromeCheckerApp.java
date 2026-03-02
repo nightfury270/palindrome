caa60976ea5351b82e1c298cd8e46547b9bebc02
nightfury270 <khc5207@gmail.com>
2026-03-02 15:25:16 +0530

    Added README file

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // loop only till half of the string
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+    }
+}
